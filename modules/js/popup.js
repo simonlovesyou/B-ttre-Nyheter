8b9fd3307b851d69be4e7d858446bca3efca7426
Simon Johansson <SimonLJohansson92@gmail.com>
2015-10-16 13:32:59 +0900

    add logic for iterating through the website and retrieving articles and filtering them based on the classifier

--- modules/js/popup.js
+++ modules/js/popup.js
@@ -1,53 +1,42 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+window.startExtension = function() {
+  var articles = document.querySelectorAll('article > a > h2');
+  console.log("Found %s number of articles!", articles.length);
+  var clickbaitCount = 0,
+      articleCount = 0;
+  var clickbaitFiltered = [],
+      articlesN = [];
 
-/**
- * Get the current URL.
- *
- * @param {function(string)} callback - called when the URL of the current tab
- *   is found.
- */
-function getCurrentTabUrl(callback) {
-  // Query filter to be passed to chrome.tabs.query - see
-  // https://developer.chrome.com/extensions/tabs#method-query
-  var queryInfo = {
-    active: true,
-    currentWindow: true
-  };
+  var filterPlus = true;
+  var filterBait = true;
 
-  chrome.tabs.query(queryInfo, function(tabs) {
-    // chrome.tabs.query invokes the callback with a list of tabs that match the
-    // query. When the popup is opened, there is certainly a window and at least
-    // one tab, so we can safely assume that |tabs| is a non-empty array.
-    // A window can only have one active tab at a time, so the array consists of
-    // exactly one tab.
-    var tab = tabs[0];
+  [].forEach.call(document.querySelectorAll('article > a'), function(v,i,a) {
+      if(v.className.indexOf('abTeaserImageLink-js') < 0 && v.className.indexOf('abStreamerSmall') < 0 && v.parentNode.className.indexOf('abItemHLine') < 0) {
+        console.log(v.textContent);
+        console.log(typeof window.isClickbait(v.textContent));
 
-    // A tab is a plain object that provides information about the tab.
-    // See https://developer.chrome.com/extensions/tabs#type-Tab
-    var url = tab.url;
+        if(filterBait && window.isClickbait(v.textContent)) {
+          if(v.parentNode) {
+            clickbaitCount++;
+            clickbaitFiltered.push(v.textContent);
+            console.log(v.parentNode);
+            v.parentNode.parentNode.removeChild(v.parentNode);
+          }
+        } else {
+          articlesN.push(v.textContent);
+          articleCount++;
+        }
+      } else {
+        console.log(v.getElementsByClassName());
+      }
 
-    // tab.url is only available if the "activeTab" permission is declared.
-    // If you want to see the URL of other tabs (e.g. after removing active:true
-    // from |queryInfo|), then the "tabs" permission is required to see their
-    // "url" properties.
-    console.assert(typeof url == 'string', 'tab.url should be a string');
 
-    callback(url);
+      /*console.log(window.isClickbait);
+      console.log(v.textContent);
+      console.log(window.isClickbait(v.textContent));*/
   });
-
-  // Most methods of the Chrome extension APIs are asynchronous. This means that
-  // you CANNOT do something like this:
-  //
-  // var url;
-  // chrome.tabs.query(queryInfo, function(tabs) {
-  //   url = tabs[0].url;
-  // });
-  // alert(url); // Shows "undefined", because chrome.tabs.query is async.
+  console.log("%s of %s are clickbait. %s%", clickbaitCount, articles.length, (clickbaitCount/articles.length*100));
+  console.log("Filtered articles:");
+  console.log(clickbaitFiltered);
+  console.log("Not filtered articles:");
+  console.log(articlesN);
 }
-
-
-document.addEventListener('DOMContentLoaded', function() {
-  alert("Hej!");
-});
